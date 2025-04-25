3070cd1c0b19f762ea1d0c9c1aa5985ea109544c
taufardh <ardhwood@gmail.com>
2025-04-25 14:44:09 +0700

    add bowling, 8 pool, monkey mart

---
+++ game/3d-bowling/js/analytics_games235.js
@@ -0,0 +1,39 @@
+function inFrame () {
+  try {
+      return window.self !== window.top;
+  } catch (e) {
+      return true;
+  }
+}
+
+function botBrowser() {
+  try {
+    return navigator.webdriver
+  } catch (e) {
+      return true;
+  }
+}
+
+function loadGoogleAnalytics(id) {
+  // Google tag (gtag.js)
+  var firstScript= document.getElementsByTagName("script")[0];
+  newScript= document.createElement("script");
+  newScript.async= "";
+  newScript.src= "https://www.googletagmanager.com/gtag/js?id="+ id;
+  firstScript.parentNode.insertBefore(newScript, firstScript);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', id);
+}
+
+window.addEventListener("load", (event) => {
+  if (botBrowser()) {
+    loadGoogleAnalytics("G-LE1ZGTPC77");
+    console.log('Bot Browser', event);
+  } else {
+    console.log('Human Browser', event);
+    loadGoogleAnalytics("G-9TFK9146KD");
+  }
+});
